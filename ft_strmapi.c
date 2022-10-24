a77cded1f1b89fa35af1eaf62b9c22f6c0c06180
Khalid Zerri <kzerri@e2r3p14.1337.ma>
2022-10-24 23:57:15 +0100

    update this func

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -1,34 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kzerri <kzerri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/24 23:54:57 by kzerri            #+#    #+#             */
-/*   Updated: 2022/10/24 23:55:23 by kzerri           ###   ########.fr       */
+/*   Created: 2022/10/18 17:49:36 by kzerri            #+#    #+#             */
+/*   Updated: 2022/10/24 01:38:15 by kzerri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-	t_list	*P;
-	t_list	*n;
+	int		i;
+	char	*p;
 
-	p = (t_list*)malloc(sizeof(t_list));
-	if (!p || !lst || !f)
+	i = 0;
+	p = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
+	if (!p || !s)
 		return (NULL);
-	while (lst)
+	while (s[i])
 	{
-		if (!(p = ft_lstnew(f(lst->content))
-		{
-			ft_lstclear(&p, del);
-			return (NULL);
-		}
-		ft_addback(&n, p);
-		lst = lst->next;
+		p[i] = f(i, s[i]);
+		i++;
 	}
-	return (n);
+	p[i] = '\0';
+	return (p);
 }
